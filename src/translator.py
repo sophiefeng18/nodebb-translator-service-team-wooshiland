08dd8690ba41cf63771032e76fa35d0e4f10d648
sophiefeng18 <149734868+sophiefeng18@users.noreply.github.com>
2024-11-15 15:57:14 -0500

    Change api endpoint in translator.py to match the updated api key

--- src/translator.py
+++ src/translator.py
@@ -8,7 +8,7 @@ print(type(os.environ.get('AZURE_OPENAI_API_KEY')))
 client = AzureOpenAI(
     api_key= os.environ.get('AZURE_OPENAI_API_KEY'),
     api_version="2024-02-15-preview",
-    azure_endpoint="https://4project.openai.azure.com/"
+    azure_endpoint="https://sophiefeng18.openai.azure.com/"
 )
 import openai
 def get_translation(post: str) -> str:
