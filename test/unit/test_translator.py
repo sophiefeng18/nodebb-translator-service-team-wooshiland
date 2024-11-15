9cf7d4b7eb31d22dc18a86fe07293720c6843389
kmehta5 <115762074+kmehta5@users.noreply.github.com>
2024-11-15 00:16:24 -0500

    Integrated LLM translator (#2)
    * Implemented LLM
    
    * Updated tests for llm
    
    * Edited prompt for punctuation
    
    * fixed flakey test
    
    * Fixed prompt to be more robust
    
    * Fixing mock

--- test/unit/test_translator.py
+++ test/unit/test_translator.py
@@ -4,15 +4,14 @@ from src.translator import translate_content
 def test_chinese():
     is_english, translated_content = translate_content("这是一条中文消息")
     assert is_english == False
-    assert translated_content == "This is a Chinese message"
+    assert translated_content == "This is a Chinese message."
 
 def test_llm_normal_response():
     is_english, translated_content = translate_content("This is an English message")
     assert is_english == True
-    assert translated_content == "This is an English message"
+    assert translated_content == ""
 
 def test_llm_gibberish_response():
     is_english, translated_content = translate_content("xyz&^%$#")
     assert is_english == True
-    assert translated_content == "xyz&^%$#"
-
+    assert translated_content == ""
