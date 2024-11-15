a915f3237edfb39056130b386ed5ce5b76fc9038
J0nathanLai <112527440+J0nathanLai@users.noreply.github.com>
2024-11-15 15:14:00 -0500

    Fix testcases

--- test/unit/test_translator.py
+++ test/unit/test_translator.py
@@ -1,12 +1,9 @@
 from src.translator import translate_content
-from dotenv import load_dotenv
 from openai import AzureOpenAI
 import os
 
-load_dotenv()
-
 client = AzureOpenAI(
-    api_key=os.getenv("API_KEY"),  
+    api_key=os.environ.get("API_KEY"),  
     api_version="2024-02-15-preview",
     azure_endpoint="https://4project.openai.azure.com/"  
 )
@@ -14,14 +11,14 @@ client = AzureOpenAI(
 def test_chinese():
     is_english, translated_content = translate_content("这是一条中文消息")
     assert is_english == False
-    assert translated_content == "This is a Chinese message."
+    assert translated_content == "This is a Chinese message"
 
 def test_llm_normal_response():
     is_english, translated_content = translate_content("This is an English message")
     assert is_english == True
-    assert translated_content == ""
+    assert translated_content == "This is an English message"
 
 def test_llm_gibberish_response():
     is_english, translated_content = translate_content("xyz&^%$#")
     assert is_english == True
-    assert translated_content == ""
+    assert translated_content == "xyz&^%$#"
