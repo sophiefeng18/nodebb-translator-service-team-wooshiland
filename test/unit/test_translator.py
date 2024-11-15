ff4f4226aadf69b06fd5a63935f591042f58ef2b
kmehta5 <115762074+kmehta5@users.noreply.github.com>
2024-11-14 23:33:04 -0500

    fixed flakey test

--- test/unit/test_translator.py
+++ test/unit/test_translator.py
@@ -4,7 +4,7 @@ from src.translator import translate_content
 def test_chinese():
     is_english, translated_content = translate_content("这是一条中文消息")
     assert is_english == False
-    assert translated_content == "This is a Chinese message"
+    assert translated_content == "This is a Chinese message."
 
 def test_llm_normal_response():
     is_english, translated_content = translate_content("This is an English message")
